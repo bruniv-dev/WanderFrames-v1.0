4cad42f9cf4744484646e31e68f902a10ee8f3a1
= <bruniv.dev@gmail.com>
2024-09-03 14:40:37 +0530

    render-frontend

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,7 @@ import postRouter from "./routers/post-routes.js"; // http://localhost:3000/post
 const allowedOrigins = [
   "http://localhost:3000", // Development frontend
   "https://wanderframes.netlify.app", // Production frontend
+  "https://wanderframes-v1-0.onrender.com",
 ];
 
 // Middleware setup
